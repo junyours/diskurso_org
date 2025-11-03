f1e8b901e2d7b2c7dfd93e6aed2031d6b672eed9
ac-devs19 <ac.devs.07192002@gmail.com>
2025-11-03 09:00:35 +0800

    group editorial

--- resources/views/pages/web/editorial-board.blade.php
+++ resources/views/pages/web/editorial-board.blade.php
@@ -1,23 +1,29 @@
 @extends('layouts.web')
 
 @section('content')
-  <div class="space-y-4">
+  <div class="space-y-6">
     <h1 class="font-bold text-lg">Editorial Board</h1>
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
-      @foreach ($editors as $editor)
-        <div class="border border-gray-200">
-          <img src="https://lh3.googleusercontent.com/d/{{ $editor->profile_picture }}" alt="{{ $editor->name }}"
-            class="object-cover w-full h-44">
-          <div class="p-2 space-y-2">
-            <div class="space-y-1">
-              <h1 class="font-semibold">{{ $editor->position }}</h1>
-              <h2 class="font-medium">{{ $editor->name }}</h2>
-              <h3 class="text-xs">{{ $editor->email }}</h3>
+    @php
+      $groupedEditors = $editors->groupBy('position');
+    @endphp
+    @foreach ($groupedEditors as $position => $group)
+      <div class="divide-y divide-gray-300">
+        <h2 class="font-semibold mb-2">{{ $position }}</h2>
+        <div class="grid sm:grid-cols-2 md:grid-cols-3 gap-2">
+          @foreach ($group as $editor)
+            <div class="border border-gray-200">
+              <img src="https://lh3.googleusercontent.com/d/{{ $editor->profile_picture }}" alt="{{ $editor->name }}"
+                class="object-cover w-full h-48">
+              <div class="p-2 flex flex-col gap-2">
+                <h2 class="font-medium break-words">{{ $editor->name }}</h2>
+                <a href="mailto:{{ $editor->email }}"
+                  class="text-xs break-words text-[#0048AE] hover:underline">{{ $editor->email }}</a>
+                <p class="text-gray-600 break-words">{{ $editor->department }}</p>
+              </div>
             </div>
-            <p>{{ $editor->department }}</p>
-          </div>
+          @endforeach
         </div>
-      @endforeach
-    </div>
+      </div>
+    @endforeach
   </div>
 @endsection
