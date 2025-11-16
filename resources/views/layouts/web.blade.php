885d8e094791154c957a4cf455deb7d9c847698c
ac-devs19 <ac.devs.07192002@gmail.com>
2025-11-16 18:13:25 +0800

    remove csrf token in web layout

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -4,7 +4,6 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
   <link rel="icon" type="image/x-icon" href="{{ asset('images/icon.png') }}">
   <meta name="google-site-verification" content="2XyQMgn8Xz_5JVc5z57O-9Aj9VNKq4-Z5KMSv-WzJgg" />
   @yield('meta')
