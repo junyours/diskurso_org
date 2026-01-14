b6d24443be14083331753a1f1720e5866bb6e248
ac-devs19 <ac.devs.07192002@gmail.com>
2026-01-14 15:48:14 +0800

    modify about publisher

--- resources/views/pages/web/category/about-publisher.blade.php
+++ resources/views/pages/web/category/about-publisher.blade.php
@@ -4,53 +4,45 @@
   <div class="text-justify space-y-4">
     <h1 class="font-bold text-lg">About the Publisher</h1>
     <div class="space-y-2">
-      <p>The journal title ‘DisKURSO International Multidisciplinary Research Journal’ embodies the institution’s
-        dedication
-        to promoting interdisciplinary research and scholarly collaboration. The journal aims to serve as a platform where
-        academics, researchers, and professionals from diverse fields can present their studies, perspectives, and
-        innovations. Published by Zas Digital Institute Training and Development Services, it highlights quality research
-        across various disciplines, including education, social sciences, technology, health, business, and more. By
-        encouraging knowledge sharing and dialogue among scholars, practitioners, and policymakers, the journal plays a
-        crucial role in advancing academic discourse. Upholding a stringent peer-review process and high scholarly
-        standards, the DisKURSO International Multidisciplinary Research Journal seeks to make a meaningful contribution
-        to
-        the global research landscape.</p>
+      <p>The DisKURSO International Multidisciplinary Research Journal reflects the institution’s commitment of Tagoloan
+        Community College through the Center for Institutional Research Development to fostering cross-disciplinary
+        research and facilitating academic exchange. As a respected publication, it provides a platform for scholars,
+        researchers, and professionals from various fields to share their findings, insights, and innovations. This
+        journal is published by Zas Digital Institute Training and Development Services (DITADS). The journal focuses on
+        the dissemination of high-quality research spanning a broad array of disciplines, including but not limited to
+        economics, business management, public administration, and other related fields.</p>
       <p>
-        The journal serves as a vital resource for advancing knowledge and facilitating dialogue
-        among researchers, practitioners, and policymakers. By embracing interdisciplinary research,
-        DisKURSO aims to address the complex issues of today’s global society. It seeks to provide
-        well-rounded solutions and perspectives by integrating ideas from various sectors of
-        knowledge, enabling more holistic approaches to solving contemporary challenges.
+        The journal serves as a vital resource for advancing knowledge and facilitating dialogue among researchers,
+        practitioners, and policymakers. By embracing interdisciplinary research, DisKURSO aims to address the complex
+        issues of today’s global society. It seeks to provide well-rounded solutions and perspectives by integrating ideas
+        from various sectors of knowledge, enabling more holistic approaches to solving contemporary challenges.
       </p>
       <p>
-        Through its rigorous peer-review process and commitment to academic excellence, the
-        DisKURSO International Multidisciplinary Research Journal aims to contribute significantly
-        to the global research community. The journal offers valuable insights into contemporary
-        challenges and solutions, pushing the boundaries of academic inquiry in its focus areas. By
-        providing a forum for the exchange of diverse ideas and research, it continues to promote
+        Through its rigorous peer-review process and commitment to academic excellence, the DisKURSO International
+        Multidisciplinary Research Journal aims to contribute significantly to the global research community. The journal
+        offers valuable insights into contemporary challenges and solutions, pushing the boundaries of academic inquiry in
+        its focus areas. By providing a forum for the exchange of diverse ideas and research, it continues to promote
         innovation, foster intellectual growth, and inspire new thinking across disciplines.
       </p>
     </div>
     <div>
-      <h1>Zas Digital Institute Training and Development Services (DITADS) is a duly registered
-        entity under Philippine law, demonstrating its commitment to transparency and compliance
-        with the necessary legal frameworks. The organization operates with the following official
-        credentials:</h1>
+      <h1>Zas Digital Institute Training and Development Services (DITADS) is a duly registered entity under Philippine
+        law, demonstrating its commitment to transparency and compliance with the necessary legal frameworks. The
+        organization operates with the following official credentials:</h1>
       <p><span class="font-semibold">• Business Permit No.:</span> BP-33229</p>
       <p><span class="font-semibold">• Certificate No.:</span> 2019-25829</p>
       <p><span class="font-semibold">• DTI Certificate of Business Registration No.:</span> 1033992</p>
-      <p><span class="font-semibold">• PhilGEPS Registration No.:</span> 201910-17877-1307663177</p>
-      <p><span class="font-semibold">• NBDB Accreditation No.:</span> 7515</p>
-      <p><span class="font-semibold">• CDA CTPro No.:</span> 271</p>
+      <p><span class="font-semibold">• Philgeps No:</span> 201910-17877-1307663177</p>
+      <p><span class="font-semibold">• NBDB No.:</span> 7515</p>
     </div>
     <div class="space-y-2">
-      <p>DITADS is located at R188 Metro Square, Zone 2 Iponan, Cagayan de Oro City. These
-        registrations underscore the legitimacy and compliance of the journal with Philippine business
-        regulations, ensuring full transparency, accountability, and adherence to legal frameworks.</p>
+      <p>DITADS is located at R188 Metro Square, Zone 2 Iponan, Cagayan de Oro City. These registrations underscore the
+        legitimacy and compliance of the journal with Philippine business regulations, ensuring full transparency,
+        accountability, and adherence to legal frameworks.</p>
       <p>
-        With its well-established credentials and adherence to national and international
-        standards, DisKURSO stands as a cornerstone for academic excellence, bridging diverse fields
-        of study and facilitating a global exchange of knowledge and ideas.
+        With its well-established credentials and adherence to national and international standards, DisKURSO stands as a
+        cornerstone for academic excellence, bridging diverse fields of study and facilitating a global exchange of
+        knowledge and ideas.
       </p>
     </div>
   </div>
